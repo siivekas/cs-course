630cddc3877fffa53e7394196a5369ce9c3df6c4
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-08 15:14:45 +0300

    HW 04-08-2021

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name and press Enter button:");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Hi, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Good bye, {name}. Press any key to finish process.");
+            
+            Console.ReadKey();
+        }
+    }
+}

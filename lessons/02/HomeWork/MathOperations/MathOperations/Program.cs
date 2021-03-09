a4ffde2d6bcf82b60e01332c9d326c69823f08e5
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-09 23:29:38 +0300

    HW 09-08-2021

---
+++ lessons/02/HomeWork/MathOperations/MathOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            double number1;
+            double number2;
+            if (double.TryParse(
+                Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
+                NumberStyles.Float,
+                CultureInfo.InvariantCulture,
+                out number1
+            ))
+            {
+                Console.WriteLine("Enter one more number:");
+                if (double.TryParse(
+                    Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
+                    NumberStyles.Float,
+                    CultureInfo.InvariantCulture,
+                    out number2
+                ))
+                {
+                    Console.WriteLine($"Сумма равна {number1 + number2}");
+                    Console.WriteLine($"Разность равна {number1 - number2}");
+                    Console.WriteLine($"Произведение равно {number1 * number2}");
+                    Console.WriteLine($"Частное равно {number1 / number2}");
+                }
+                else
+                {
+                    Console.WriteLine("Do not enter letters, only numbers!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do not enter letters, only numbers!");
+            }
+        }
+    }
+}

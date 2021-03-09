696f2830b9d3810435e2ad5126b36e1449e5aced
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-10 00:17:09 +0300

    HW 09-03-2021 усложненный вариант

--- lessons/02/HomeWork/MathOperations/MathOperations/Program.cs
+++ lessons/02/HomeWork/MathOperations/MathOperations/Program.cs
@@ -9,7 +9,7 @@ namespace MathOperations
         {
             Console.WriteLine("Enter number:");
             double number1;
-            double number2;
+            // проверяем и кастуем в дабл ввод юзера
             if (double.TryParse(
                 Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
                 NumberStyles.Float,
@@ -18,6 +18,8 @@ namespace MathOperations
             ))
             {
                 Console.WriteLine("Enter one more number:");
+                double number2;
+                // проверяем и кастуем в дабл второй ввод юзера
                 if (double.TryParse(
                     Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
                     NumberStyles.Float,
@@ -25,10 +27,51 @@ namespace MathOperations
                     out number2
                 ))
                 {
-                    Console.WriteLine($"number1 + number2 = {number1 + number2}");
-                    Console.WriteLine($"number1 - number2 = {number1 - number2}");
-                    Console.WriteLine($"number1 * number2 = {number1 * number2}");
-                    Console.WriteLine($"number1 / number2 = {number1 / number2}");
+                    // красиво выводим меню
+                    string[] phrases =
+                    {
+                        "Choose operation type:",
+                        "0 - addition (+)",
+                        "1 - subtraction (-)",
+                        "2 - multiplication (*)",
+                        "3 - division (/)",
+                        "4 - exponentiation (^)"
+                    };
+                    foreach (string phrase in phrases)
+                    {
+                        Console.WriteLine(phrase);
+                    }
+                    // проверяем и кастуем в инт выбор юзера
+                    int operationType;
+                    if (
+                        int.TryParse(Console.ReadLine(), out operationType) &&
+                        operationType >= 0 &&
+                        operationType <= 4
+                    )
+                    {
+                        // выводим результат
+                        switch (operationType)
+                        {
+                            case 0: Console.WriteLine($"Sum equals to {number1 + number2}");
+                                break;
+                            case 1: Console.WriteLine($"Difference equals to {number1 - number2}");
+                                break;
+                            case 2: Console.WriteLine($"Composition equals to {number1 * number2}");
+                                break;
+                            case 3: Console.WriteLine($"Quotient equals to {number1 / number2}");
+                                break;
+                            case 4: Console.WriteLine($"{number1} raised to the power of {number2} equals to {Math.Pow(number1, number2)}");
+                                break;
+                        }
+                        
+                        // подтверждение завершения операции
+                        Console.WriteLine("Press eny key to finish process");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choose valid operation type");
+                    }
                 }
                 else
                 {

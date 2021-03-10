4e22a1207b3ee0374f71faf38617caac689c794a
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-11 00:06:11 +0300

    Вынесла методы, поправила деление на ноль, убрала лишние условия.

--- lessons/02/HomeWork/MathOperations/MathOperations/Program.cs
+++ lessons/02/HomeWork/MathOperations/MathOperations/Program.cs
@@ -8,83 +8,88 @@ namespace MathOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Enter number:");
-            double number1;
-            // проверяем и кастуем в дабл ввод юзера
-            if (double.TryParse(
+            if (!TryReadDouble(out double number1))
+            {
+                ShowError("Do not enter letters, only numbers!");
+                return;
+            }
+
+            Console.WriteLine("Enter one more number:");
+            if (!TryReadDouble(out double number2))
+            {
+                ShowError("Do not enter letters, only numbers!");
+                return;
+            }
+
+            string[] phrases =
+            {
+                "Choose operation type:",
+                "0 - addition (+)",
+                "1 - subtraction (-)",
+                "2 - multiplication (*)",
+                "3 - division (/)",
+                "4 - remainder of division (%)",
+                "5 - exponentiation (^)"
+            };
+            foreach (string phrase in phrases)
+            {
+                Console.WriteLine(phrase);
+            }
+
+            if (!int.TryParse(Console.ReadLine(), out int operationType))
+            {
+                ShowError("Do not enter letters, only numbers!");
+                return;
+            }
+
+            switch (operationType)
+            {
+                case 0:
+                    Console.WriteLine($"Sum equals to {number1 + number2}");
+                    break;
+                case 1:
+                    Console.WriteLine($"Difference equals to {number1 - number2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Composition equals to {number1 * number2}");
+                    break;
+                case 3:
+                    string message = (number2 == 0)
+                        ? "Can`t divide by zero "
+                        : $"Quotient equals to {number1 / number2}";
+                    Console.WriteLine(message);
+                    break;
+                case 4:
+                    Console.WriteLine($"Remainder of division equals to {number1 % number2}");
+                    break;
+                case 5:
+                    Console.WriteLine(
+                        $"{number1} raised to the power of {number2} equals to {Math.Pow(number1, number2)}");
+                    break;
+                default:
+                    Console.WriteLine("Undefined operation type");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to finish process");
+            Console.ReadKey();
+        }
+
+        static bool TryReadDouble(out double number)
+        {
+            return double.TryParse(
                 Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
                 NumberStyles.Float,
                 CultureInfo.InvariantCulture,
-                out number1
-            ))
-            {
-                Console.WriteLine("Enter one more number:");
-                double number2;
-                // проверяем и кастуем в дабл второй ввод юзера
-                if (double.TryParse(
-                    Console.ReadLine().Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator),
-                    NumberStyles.Float,
-                    CultureInfo.InvariantCulture,
-                    out number2
-                ))
-                {
-                    // красиво выводим меню
-                    string[] phrases =
-                    {
-                        "Choose operation type:",
-                        "0 - addition (+)",
-                        "1 - subtraction (-)",
-                        "2 - multiplication (*)",
-                        "3 - division (/)",
-                        "4 - remainder of division (%)",
-                        "5 - exponentiation (^)"
-                    };
-                    foreach (string phrase in phrases)
-                    {
-                        Console.WriteLine(phrase);
-                    }
-                    // проверяем и кастуем в инт выбор юзера
-                    int operationType;
-                    if (
-                        int.TryParse(Console.ReadLine(), out operationType) &&
-                        operationType >= 0 &&
-                        operationType <= 5
-                    )
-                    {
-                        // выводим результат
-                        switch (operationType)
-                        {
-                            case 0: Console.WriteLine($"Sum equals to {number1 + number2}");
-                                break;
-                            case 1: Console.WriteLine($"Difference equals to {number1 - number2}");
-                                break;
-                            case 2: Console.WriteLine($"Composition equals to {number1 * number2}");
-                                break;
-                            case 3: Console.WriteLine($"Quotient equals to {number1 / number2}");
-                                break;
-                            case 4: Console.WriteLine($"Remainder of division equals to {number1 % number2}");
-                                break;
-                            case 5: Console.WriteLine($"{number1} raised to the power of {number2} equals to {Math.Pow(number1, number2)}");
-                                break;
-                        }
-                        
-                        // подтверждение завершения операции
-                        Console.WriteLine("Press any key to finish process");
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Choose valid operation type");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Do not enter letters, only numbers!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Do not enter letters, only numbers!");
-            }
+                out number
+            );
+        }
+
+        static void ShowError(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Press any key to finish process");
+            Console.ReadKey();
         }
     }
 }

4071d3fbeb68305717eed739b52b4480918ae41a
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-11 22:11:51 +0300

    CW 11-03-2021

---
+++ lessons/03/ClassWork/Variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamicVar = "string";
+            dynamicVar = 5;
+            Console.WriteLine(Math.Pow(dynamicVar, 2));
+            
+            object objString = "string";
+            Console.WriteLine(objString.ToString().Length);
+            
+            var doubleVar = 5.5;
+            Console.WriteLine(doubleVar.GetType());
+            
+            char a = default(char);
+            Console.WriteLine(a);
+
+            var array = new string[4];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("---");
+            Thread.Sleep(1000);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

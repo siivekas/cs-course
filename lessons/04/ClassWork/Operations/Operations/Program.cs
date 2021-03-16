2c7111e1a91ce6012c50939f1aff398194138df8
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-16 21:12:54 +0300

    CW 16-03-2021 стороны и объем пирамиды

---
+++ lessons/04/ClassWork/Operations/Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rootThree = Math.Sqrt(3);
+            
+            Console.WriteLine("Enter h:");
+            double h = double.Parse(
+                Console.ReadLine().Replace(",", "."),
+                CultureInfo.InvariantCulture
+                );
+            
+            Console.WriteLine("Enter a:");
+            double a = double.Parse(
+                Console.ReadLine().Replace(",", "."),
+                CultureInfo.InvariantCulture
+                );
+
+            var Sside = 3 * a * h;
+            var Sfull = 1.5 * a * (a * rootThree + 2 * h);
+            var H = Math.Sqrt(Math.Pow(h, 2) - Math.Pow(a, 2) / 12);
+            var V = Math.Pow(a, 2) * H * rootThree / 2;
+            
+            Console.WriteLine($"S side = {Sside}");
+            Console.WriteLine($"S full = {Sfull}");
+            Console.WriteLine($"V = {V}");
+        }
+    }
+}

2b1148eb50d90708c84373f5e89682ecfe3a2fea
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-18 20:03:32 +0300

    CW 18-02-2021 возраст

---
+++ lessons/05/ClassWork/IfElse/IfElse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст");
+
+            string rawAge = Console.ReadLine();
+            if (!int.TryParse(rawAge, out int age))
+            {
+                Console.WriteLine("Это не возраст");
+                return;
+            }
+
+            string label;
+            int lastChar = int.Parse(rawAge.Substring(rawAge.Length - 1));
+
+            if (age >= 10 && age <= 19)
+            {
+                label = "лет";
+            } else if (lastChar >= 5)
+            {
+                label = "лет";
+            } else if (lastChar >= 2)
+            {
+                label = "года";
+            } else
+            {
+                label = "год";
+            }
+            
+            Console.WriteLine($"{age} {label}");
+        }
+    }
+}

8ff30d9604dec01a1268430c23c1c072cade53e3
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-23 14:43:58 +0300

    HW 16-03-2021

---
+++ lessons/04/HomeWork/ColorPicker/ColorPicker/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ColorPicker
+{
+    [Flags]
+    public enum Colors
+    {
+        Black = 0x1,
+        Blue = 0x1 << 1,
+        Cyan = 0x1 << 2,
+        Grey = 0x1 << 3,
+        Green = 0x1 << 4,
+        Magenta = 0x1 << 5,
+        Red = 0x1 << 6,
+        White = 0x1 << 7,
+        Yellow = 0x1 << 8
+    }
+}
---
+++ lessons/04/HomeWork/ColorPicker/ColorPicker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ColorPicker
+{
+    class Program
+    {
+        private const int SelectedNum = 4;
+        private const int TryNum = 2;
+        
+        static void Main(string[] args)
+        {
+            Colors notSelected = default;
+            Colors selected = default;
+            Console.WriteLine($"Choose {SelectedNum} colors:");
+            foreach (Colors color in Enum.GetValues(typeof(Colors)))
+            {
+                notSelected = notSelected | color;
+                Console.WriteLine($"{(int)color} - {color}");
+            }
+
+            for (int i = 0; i < SelectedNum; i++)
+            {
+                Console.WriteLine($"Color №{i+1}:");
+                for (int triesLeft = TryNum; triesLeft > 0; triesLeft--)
+                {
+                    if (Enum.TryParse(Console.ReadLine(), out Colors userInput) &&
+                        Enum.IsDefined(typeof(Colors), userInput))
+                    {
+                        selected = selected | userInput;
+                        notSelected = notSelected ^ userInput;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input, try again (color №{i+1}, tries left: {triesLeft-1})");
+                    }
+                }
+            }
+            Console.WriteLine($"Selected colors: {selected}");
+            Console.WriteLine($"Not selected colors: {notSelected}");
+        }
+    }
+}

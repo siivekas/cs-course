d62dbb565c8dc69a584aec7d1aa177fb33912225
siivekas <80065455+siivekas@users.noreply.github.com>
2021-03-23 19:28:03 +0300

    CW 23-03-2021 ввод строки, пока не exit (#3)

---
+++ lessons/06/ClassWork/Cycles/Cycles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("Enter some string. Entering \"exit\" will finish process.");
+            do
+            {
+                userInput = Console.ReadLine();
+            } while (userInput != "exit");
+        }
+    }
+}

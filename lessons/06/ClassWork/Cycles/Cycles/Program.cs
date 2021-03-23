15ddda5c46d6bc8da606746bbe9a5c21b9ac9b55
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-23 20:55:37 +0300

    CW 23-03-2021 оценки

--- lessons/06/ClassWork/Cycles/Cycles/Program.cs
+++ lessons/06/ClassWork/Cycles/Cycles/Program.cs
@@ -6,15 +6,40 @@ namespace Cycles
     {
         static void Main(string[] args)
         {
-            int[] ar = {1, 2, 3, 4, 5, 6, 7};
-            int i= 0;
-            int sum = 0;
-            while (i < ar.Length)
+            var marks = new[]
             {
-                sum += ar[i];
-                Console.WriteLine($"Now sum is {sum}");
-                i++;
+                new[] {2, 3, 3, 3},
+                new[] {5, 2, 3},
+                null,
+                new[] {5, 5, 5, 5},
+                new[] {4}
+            };
+
+            double weekSum = 0.0;
+            int marksNum = 0;
+            for (int weekDay = 0; weekDay < marks.Length; weekDay++)
+            {
+                double daySum = 0.0;
+                try
+                {
+                    foreach (int mark in marks[weekDay])
+                    {
+                        marksNum++;
+                        daySum += mark;
+                    }
+                    weekSum += daySum;
+                    Console.WriteLine(
+                        $"The average mark for day #{weekDay + 1}: {Math.Round(daySum / marks[weekDay].Length, 1)}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"The average mark for day #{weekDay + 1}: N/A");
+                    continue;
+                }
             }
+
+            Console.WriteLine(
+                $"The average mark for whole week: {Math.Round(weekSum / marksNum, 1)}");
         }
     }
 }

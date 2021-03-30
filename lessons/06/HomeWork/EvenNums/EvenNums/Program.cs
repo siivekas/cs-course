29febfbf18a939c05f2101b8c9e8a6623c3dd485
Helga Helvegen <h.helvegen@yandex.ru>
2021-03-30 14:20:50 +0300

    HW 24-03-2021 четные числа

---
+++ lessons/06/HomeWork/EvenNums/EvenNums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter some positive integer or \"exit\" to exit process");
+                try
+                {
+                    string userInput = Console.ReadLine();
+                    
+                    if(userInput == "exit") break;
+                    
+                    if(int.Parse(userInput) <= 0) throw new ArgumentException();
+                    
+                    int evenNum = 0;
+                    for (int i = 0; i < userInput.Length; i++)
+                    {
+                        if (userInput[i] % 2 == 0) evenNum++;
+                    }
+                    
+                    Console.WriteLine($"In number {userInput} there is {evenNum} even number(s)");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Non numeric input");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Too long number");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Negative or zero number");
+                }
+            }
+        }
+    }
+}

9127de42f7c61f023470956bab00769420761d88
Helga Helvegen <h.helvegen@yandex.ru>
2021-04-01 03:19:36 +0300

    HW 25-03-2021 строка в нижнем регистре в обратном порядке

---
+++ lessons/07/HomeWork/NormalizeAndReverse/NormalizeAndReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace NormalizeAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some string:");
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Empty string, try again");
+                    continue;
+                }
+                
+                Console.WriteLine(userInput.ToLower().Reverse().ToArray());
+                break;
+            }
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}

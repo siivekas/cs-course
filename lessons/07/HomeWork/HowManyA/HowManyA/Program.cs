b83c1a58de5273c5f40e42416d8b2d85907beccb
Helga Helvegen <h.helvegen@yandex.ru>
2021-04-01 03:06:55 +0300

    HW 25-03-2021 слова на А

---
+++ lessons/07/HomeWork/HowManyA/HowManyA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HowManyA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из как минимум двух слов:");
+
+            int count = 0;
+            while (true)
+            {
+                string[] userInputArray = Console.ReadLine().ToLower().Split(new[] {' ', '\t'},
+                    StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                if (userInputArray.Length < 2)
+                {
+                    Console.WriteLine("Слишком мало слов, попробуйте еще раз.");
+                    continue;
+                }
+
+                foreach (string str in userInputArray)
+                    if (str[0].ToString().Equals("а", StringComparison.OrdinalIgnoreCase))
+                        count++;
+                break;
+            }
+
+            Console.WriteLine($"Слов, начинающихся с буквы \"а\" (кириллица): {count}");
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}

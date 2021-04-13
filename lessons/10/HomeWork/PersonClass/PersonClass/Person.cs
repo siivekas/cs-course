d0302888cefb8b43484030a2766ff3896581efea
Helga Helvegen <h.helvegen@yandex.ru>
2021-04-13 18:17:48 +0300

    HW 07-04-2021 класс Person

---
+++ lessons/10/HomeWork/PersonClass/PersonClass/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PersonClass
+{
+    public class Person
+    {
+        private string _mName;
+        private int _mAgeYears;
+
+        public string Name
+        {
+            get { return _mName; }
+            set { _mName = value[0].ToString().ToUpper() + value.Substring(1); }
+        }
+
+        public int AgeYears
+        {
+            get { return _mAgeYears; }
+            set
+            {
+                if (value >= 0) _mAgeYears = value;
+                else throw new ArgumentOutOfRangeException(); // можно возвращать модуль, но я хочу попробовать так
+            }
+        }
+
+        public int AgeIn4
+        {
+            get { return AgeYears + 4; }
+        }
+
+        public string Description
+        {
+            get { return $"Имя: {Name}, полных лет: {AgeYears}, через 4 года: {AgeIn4}"; }
+        }
+        
+        public static string InputPersonName(int number = 0)
+        {
+            Console.WriteLine($"Введите имя пользователя {number + 1}:");
+            string userInputName = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userInputName))
+                throw new ArgumentNullException();
+
+            return userInputName;
+        }
+
+        public static int InputPersonAge(int number = 0)
+        {
+            int userAge;
+            Console.WriteLine($"Введите возраст пользователя {number + 1}:");
+            string userInputAge = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userInputAge)) throw new ArgumentNullException();
+
+            userAge = int.Parse(userInputAge);
+            if (userAge < 0) throw new ArgumentOutOfRangeException();
+
+            return userAge;
+        }
+    }
+}
---
+++ lessons/10/HomeWork/PersonClass/PersonClass/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personArray = new List<Person>(); // чтобы количество человек тоже сделать динамическим
+            int personCount = GetPersonCount(); // чисто для красоты, чтобы обычный бесконечный цикл ошибок не занимал пол-экрана
+
+            for (int i = 0; i < personCount; i++)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        personArray.Add(new Person()
+                        {
+                            Name = Person.InputPersonName(i),
+                            AgeYears = Person.InputPersonAge(i)
+                        });
+                        break;
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        ShowColoredMessage("Ничего не введено, попробуйте еще раз.");
+                    }
+                    catch (FormatException)
+                    {
+                        ShowColoredMessage("Не могу распознать число, попробуйте еще раз.");
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        ShowColoredMessage("Введено неположительное значение, попробуйте еще раз.");
+                    }
+                }
+            }
+
+            foreach (var person in personArray)
+            {
+                Console.WriteLine(person.Description);
+                ShowColoredMessage(person.Description, ConsoleColor.Green);
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+
+        static int GetPersonCount()
+        {
+            int personCount;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Сколько человек добавить?");
+                    string userInputCount = Console.ReadLine();
+                    if (userInputCount is null)
+                    {
+                        ShowColoredMessage("Ничего не введено, попробуйте еще раз.");
+                        continue;
+                    }
+
+                    personCount = int.Parse(userInputCount);
+                    if (personCount <= 0)
+                    {
+                        ShowColoredMessage("Введено неположительное число, попробуйте еще раз.");
+                        continue;
+                    }
+
+                    break;
+                }
+                catch (FormatException)
+                {
+                    ShowColoredMessage("Не могу распознать число, попробуйте еще раз.");
+                }
+            }
+
+            return personCount;
+        }
+
+        static void ShowColoredMessage(string message, ConsoleColor color = ConsoleColor.Red)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}

27016999df40c3a20ad749a095dad009ada1b9a0
Helga Helvegen <h.helvegen@yandex.ru>
2021-04-13 20:11:31 +0300

    CW 13-04-2021 virtual/override

--- lessons/12/ClassWork/Extension/Extension/BaseDocument.cs
+++ lessons/12/ClassWork/Extension/Extension/BaseDocument.cs
@@ -8,7 +8,7 @@ namespace Extension
         public string Number { get; set; }
         public DateTimeOffset IssueDate { get; set; }
 
-        public string Description => $"Type {GetType()} \n" +
+        public virtual string Description => $"Type {GetType()} \n" +
                                      $"Title {Title} \n" +
                                      $"Number {Number} \n" +
                                      $"Issue date {IssueDate} \n";
--- lessons/12/ClassWork/Extension/Extension/Passport.cs
+++ lessons/12/ClassWork/Extension/Extension/Passport.cs
@@ -7,13 +7,11 @@ namespace Extension
         public string Country { get; set; }
         public string PersonName { get; set; }
         
-        public new string Description => $"Type {GetType()} \n" +
+        public override string Description => $"Type {GetType()} \n" +
                                      $"Title {Title} \n" +
                                      $"Number {Number} \n" +
                                      $"Issue date {IssueDate} \n" +
                                      $"Country {Country} \n" +
                                      $"Owners`s name {PersonName} \n";
-        
-        public new void WriteToConsole() => Console.WriteLine(Description);
     }
 }

e67882b0f660dc9d43a20537fce6e51ebcc82999
Helga Helvegen <h.helvegen@yandex.ru>
2021-04-15 14:29:40 +0300

    HW 08-04-2021 класс будильника

---
+++ lessons/11/HomeWork/Reminder/Reminder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ri1 = new ReminderItem(
+                DateTimeOffset.Now.AddDays(-3),
+                "Alarm 1"
+            );
+            var ri2 = new ReminderItem(
+                DateTimeOffset.Now.AddDays(+2),
+                "Alarm 2"
+            );
+            
+            ri1.WriteProperties();
+            ri2.WriteProperties();
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
